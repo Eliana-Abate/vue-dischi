b2871e0803150b5d4621fbc93067061aa88175d7
Eliana-Abate <eliana.abate@outlook.it>
2021-09-02 16:12:44 +0200

    added axios

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ console.log ('collegato', Vue);
 const root = new Vue ({
     el: '#root', 
     data: {
+        album: [],
 
     }, 
     computed: {
@@ -12,6 +13,33 @@ const root = new Vue ({
     }, 
     methods: {
 
+    },
+
+    created() {
+
+        for (let i = 0; i < 10; i++) {
+            axios 
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((res) => {
+                const response = res.data.response;
+                console.log(response);
+                const singleDisc = {
+                    poster: '',
+                    title: '',
+                    author: '',
+                    year: '',
+                };
+
+                singleDisc.poster = response.poster;
+                singleDisc.title = response.title;
+                singleDisc.author = response.author;
+                singleDisc.year = response.year;
+
+                this.album.push(singleDisc);
+                
+                
+            })
+        }  
     },
 
 });
